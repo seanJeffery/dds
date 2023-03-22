828bd1f2d2a1606320d90022658d17dcd1e4be52
seanJeffery <sean.jeffery98@gmail.com>
2023-03-22 16:31:52 -0700

    add dds_interface header and source with create_dds_task function

---
+++ src/dds_interface.c
@@ -0,0 +1,19 @@
+/*
+ * dds_interface.c
+ *
+ *  Created on: Mar 22, 2023
+ *      Author: seanj
+ */
+
+#include "dds_interface.h"
+
+void create_dd_task( TaskHandle_t t_handle,
+					 task_type type,
+					 uint32_t task_id,
+					 uint32_t absolute_deadline
+					)
+{
+
+}
+
+
